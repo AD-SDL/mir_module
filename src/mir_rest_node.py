838424ff70930d783caa336d924eb9129afb3141
tginsbu1 <tsg44@cornell.edu>
2026-03-25 02:44:35 -0500

    updated to madsci

--- src/mir_rest_node.py
+++ src/mir_rest_node.py
@@ -3,157 +3,106 @@
 from pathlib import Path
 from typing import List, Optional
 
-from fastapi.datastructures import State
 from mir_driver.mir_driver import MiR_Base
 from typing_extensions import Annotated
-from wei.modules.rest_module import RESTModule
-from wei.types.module_types import ModuleState, ModuleStatus
-from wei.types.step_types import ActionRequest, StepResponse
-from wei.utils import extract_version
+from madsci.common.ownership import get_current_ownership_info
+from madsci.common.types.node_types import RestNodeConfig
+from madsci.node_module.helpers import action
+from madsci.node_module.rest_node_module import RestNode
+from madsci.common.types.location_types import LocationArgument
 
-rest_module = RESTModule(
-    name="mir_node",
-    version=extract_version(Path(__file__).parent.parent / "pyproject.toml"),
-    description="A node to control the mobile MIR Base",
-    model="Mir250",
-)
+class MIRConfig(RestNodeConfig):
+    """Configuration for the MIR REST node"""
+    mir_host: str = "mirbase2.cels.anl.gov"
+    map_name: str = "RPL"
 
-rest_module.arg_parser.add_argument(
-    "--mir_host",
-    type=str,
-    default="mirbase2.cels.anl.gov",
-    help="Hostname or IP address to connect to MIR Base",
-)
-rest_module.arg_parser.add_argument(
-    "--map_name",
-    type=str,
-    default="RPL",
-    help="Hostname or IP address to connect to MIR Base",
-)
+class MIRNode:
+    """A node to control the mobile MIR Base"""
+  
+    config: MIRConfig = MIRConfig()
+    config_model = MIRConfig
+    def startup_handler(self):
+        """MIR startup handler."""
+        
+        self.mir = MiR_Base(mir_ip=self.config.mir_host, map_name=self.config.map_name)
+        print("MIR Base online")
 
+    def status_handler(self):
+        """Periodically called to update the current status of the node."""
+        """Returns the current state of the UR module"""
+        if self.node_status.busy == False:
+            robot_state = self.mir.get_state()
+            if robot_state == "ERROR":
+                self.node_status.errored = True
+            elif robot_state == "EXECUTING":
+                self.node_status.busy = True
+                error = "Executing current mission, can still accept more missions."
+            elif len(self.node_status.running_actions) == 0:
+                self.node_status.busy = False
 
-@rest_module.startup()
-def mir_startup(state: State):
-    """MIR startup handler."""
-    state.mir = None
-    state.mir = MiR_Base(mir_ip=state.mir_host, map_name=state.map_name)
-    print("MIR Base online")
+            
 
-
-@rest_module.state_handler()
-def state(state: State):
-    """Returns the current state of the UR module"""
-    if state.mir.status != "BUSY":
-        robot_state = state.mir.get_state()
-        if robot_state == "ERROR":
-            state.status = ModuleStatus.ERROR  # MiR state messages do not align. IDLE should be READY, I believe. Need to run MiR to determine other state messages.
-            error = ""
-        elif robot_state == "EXECUTING":
-            state.status = ModuleStatus.IDLE
-            error = "Executing current mission, can still accept more missions."
-        else:
-            state.status = ModuleStatus.IDLE
-            error = "All missions complete."
-    else:
-        state.status = ModuleStatus.BUSY
-        error = "Waiting for other modules to finish."
-    return ModuleState(status=state.status, error=error)
+    @action
+    def move(
+        self,
+        target_location: Annotated[LocationArgument, "Target location name"],
+        description: Annotated[str, "Description of the location"],
+        priority: Annotated[Optional[int], "Prority of the movement in the queue. Default is 0."],
+    ) -> None:
+        """Sends a move command to the MIR Base"""
+        self.mir.move(
+            location_name=target_location.representation,
+        )
+        
 
 
-@rest_module.action(
-    name="move",
-    description="Send a Move command to the MIR Base",
-)
-def move(
-    state: State,
-    action: ActionRequest,
-    target_location: Annotated[List[dict], "Target location name"],
-    description: Annotated[str, "Description of the location"],
-    priority: Annotated[Optional[int], "Prority of the movement in the queue. Default is 0."],
-) -> StepResponse:
-    """Sends a move command to the MIR Base"""
-    state.mir.move(
-        location_name=target_location,
-    )
-    return StepResponse.step_succeeded()
 
 
-@rest_module.action(
-    name="wait_until_finished", description="Wait until previous mission is finished before proceeding."
-)
-def wait_until_finished(
-    state: State,
-    action: ActionRequest,
-) -> StepResponse:
-    """MIR Base continuously checks status of last sent mission before proceeding."""
-    state.mir.wait_until_finished()
-    return StepResponse.step_succeeded()
+    @action
+    def dock(self, 
+        target_location: Annotated[LocationArgument, "Name of the docking location"],
+    ) -> None:
+        """Sends a docking command to the MIR Base"""
+        self.mir.dock(
+            location_name=target_location.representation,
+        )
+        self.mir.wait_until_finished()
+
+    @action
+    def queue_mission(
+        self,
+        name: Annotated[List[float], "Name of the mission"],
+        mission: Annotated[List[dict], "A list of action dictionaries"],
+        description: Annotated[str, "Description of the mission"],
+        priority: Annotated[Optional[int], "Prority of the mission in the queue. Defult is 1"],
+    ) -> None:
+        """Sends a mission to the MIR Base which could have multiple movement actions"""
+        self.mir.post_mission_to_queue(
+            mission_name=name,
+            act_param_dict=mission,
+            description=description,
+            priority=priority,
+        )
+        self.mir.wait_until_finished()
 
 
-@rest_module.action(
-    name="dock",
-    description="Sends a dock command to the MIR Base",
-)
-def dock(
-    state: State,
-    action: ActionRequest,
-    target_location: Annotated[List[dict], "Name of the docking location"],
-) -> StepResponse:
-    """Sends a docking command to the MIR Base"""
-    state.mir.dock(
-        location_name=target_location,
-    )
-    return StepResponse.step_succeeded()
+    @action
+    def abort_mission_queue(
+        self
+    ) -> None:
+        """Aborts all the missions in the queue"""
+        self.mir.abort_mission_queue()
 
 
-@rest_module.action(
-    name="queue_mission",
-    description="Adds a new mission to the queue. A mission could have multiple movement actions",
-)
-def queue_mission(
-    state: State,
-    action: ActionRequest,
-    name: Annotated[List[float], "Name of the mission"],
-    mission: Annotated[List[dict], "A list of action dictionaries"],
-    description: Annotated[str, "Description of the mission"],
-    priority: Annotated[Optional[int], "Prority of the mission in the queue. Defult is 1"],
-) -> StepResponse:
-    """Sends a mission to the MIR Base which could have multiple movement actions"""
-    state.mir.post_mission_to_queue(
-        mission_name=name,
-        act_param_dict=mission,
-        description=description,
-        priority=priority,
-    )
-    return StepResponse.step_succeeded()
-
-
-@rest_module.action(
-    name="abort_mission_queue",
-    description="Send a abort_mission_queue command to the MIR Base",
-)
-def abort_mission_queue(
-    state: State,
-    action: ActionRequest,
-) -> StepResponse:
-    """Aborts all the missions in the queue"""
-    state.mir.abort_mission_queue()
-    return StepResponse.step_succeeded()
-
-
-@rest_module.action(
-    name="add_wait",
-    description="Send a abort_mission_queue command to the MIR Base",
-)
-def add_wait(
-    state: State,
-    action: ActionRequest,
-    delay_seconds: float,
-) -> StepResponse:
-    """Adds a wait mission to MIR Base"""
-    state.mir.wait(delay_seconds)
-    return StepResponse.step_succeeded()
+    @action
+    def add_wait(
+        self,
+        delay_seconds: float,
+    ) -> None:
+        """Adds a wait mission to MIR Base"""
+        self.mir.wait(delay_seconds)
 
 
 if __name__ == "__main__":
-    rest_module.start()
+    MIR_node = MIRNode()
+    MIR_node.start_node
